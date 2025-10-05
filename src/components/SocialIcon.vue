c01528047bb6e73cefae8bafbef1853bb4e300f7
TraumDE <eugene301workmaik@gmail.com>
2025-10-06 00:30:22 +0300

    add social icons component

---
+++ src/components/SocialIcon.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import TwitterIcon from './icons/TwitterIcon.vue'
+import FacebookIcon from './icons/FacebookIcon.vue'
+import LinkedinIcon from './icons/LinkedinIcon.vue'
+
+interface Props {
+  icon: 'twitter' | 'facebook' | 'linkedin'
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <a href="/">
+    <TwitterIcon v-if="props.icon === 'twitter'"></TwitterIcon>
+    <FacebookIcon v-else-if="props.icon === 'facebook'"></FacebookIcon>
+    <LinkedinIcon v-else-if="props.icon === 'linkedin'"></LinkedinIcon>
+  </a>
+</template>
