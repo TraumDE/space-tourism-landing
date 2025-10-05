250752d87f5420fd4d8321c0cfd4b5986b276a06
TraumDE <eugene301workmaik@gmail.com>
2025-10-06 00:25:11 +0300

    Add social icons

---
+++ src/components/icons/facebookIcon.vue
@@ -0,0 +1,8 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" width="23" height="24" viewBox="0 0 23 24">
+    <path
+      fill="#fff"
+      d="M21.7 1H1.3c-.5 0-.9.4-.9.8v20.5c0 .4.4.8 1 .8h20.3c.5 0 1-.4 1-.8V1.8c0-.4-.5-.8-1-.8Zm-2.5 6.4h-1.8c-1.4 0-1.7.7-1.7 1.7v2.1H19l-.4 3.3h-2.9v8.6h-3.4v-8.5h-3v-3.4h3V8.7c0-2.8 1.7-4.4 4.3-4.4l2.6.1v3Z"
+    />
+  </svg>
+</template>
